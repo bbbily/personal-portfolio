a8c43128b9413362c4b77541bdd64d9eee936340
Peng Wu <peng116233402@gmail.com>
2017-04-10 23:31:02 -0600

    add animation, resposive

--- public/main.js
+++ public/main.js
@@ -18,6 +18,31 @@ $(document).ready(function() {
     $("body").animate({scrollTop: $(".contact").offset().top}, 600);
   })
 
+  $(".projects-main").find("a").mouseover(function() {
+    $(this).find(".projects-overlay").fadeIn("fast", function() {
+      //Stuff to do *after* the animation takes place
+    })
+    $(this).find("h4").fadeIn("fast", function() {
+      //Stuff to do *after* the animation takes place
+    })
+    $(this).find("hr").css({"width": "100%", "opacity": "1"});
+    $(this).find("p").css({"opacity": "1", "transform": "translateX(0)"});
+    $(this).find("img").css({"transform": "translateX(10px)"})
+  })
+
+  $(".projects-main").find("a").mouseleave(function() {
+    $(this).find(".projects-overlay").fadeOut("fast", function() {
+      //Stuff to do *after* the animation takes place
+    })
+    $(this).find("h4").fadeOut("fast", function() {
+      //Stuff to do *after* the animation takes place
+    })
+    $(this).find("hr").css({"width": "0", "opacity": "0"});
+    $(this).find("p").css({"opacity": "0", "transform": "translateX(100%)"});
+    $(this).find("img").css({"transform": "translateX(0px)"})
+  })
+
+
   $window = $(window);
 
   $window.scroll(function() {
